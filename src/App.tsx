0648597232c52bc6e75929e2b65699df8a3b5040
Gleb Panteleev <otohoshi@gmail.com>
2023-10-10 17:37:45 +0400

    init ts, webpack, app and etc

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const App: React.FC = () => {
+    return (
+        <div className="wrapper">
+            <h1>React and TypeScript App!🚀</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import { Configuration, DefinePlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+
+const webpackConfig = (): Configuration => {
+    return ({
+        entry: "./src/index.tsx",
+        ...(process.env.production || !process.env.development
+            ? {}
+            : {devtool: "eval-source-map"}),
+
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+            plugins: [new TsconfigPathsPlugin({configFile: "./tsconfig.json"})],
+        },
+        output: {
+            path: path.join(__dirname, "/build"),
+            filename: "build.js",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: "ts-loader",
+                    options: {
+                        transpileOnly: true,
+                    },
+                    exclude: /build/,
+                },
+                {
+                    test: /\.s?css$/,
+                    use: ["style-loader", "css-loader"],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./public/index.html",
+            }),
+            new DefinePlugin({
+                "process.env": process.env.production || !process.env.development,
+            }),
+        ],
+    });
+};
+
+export default webpackConfig;
