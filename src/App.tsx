52c8c45f4629befef6993a7aa9b08701c8706441
Gleb Panteleev <otohoshi@gmail.com>
2023-10-10 20:59:51 +0400

    Configurations for production. TsLint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
 import './styles.scss';
 
 const App: React.FC = () => {
+    console.log(`KEY: ${process.env.REACT_APP_KEY}\nMODE: ${process.env.NODE_ENV}`);
+
     return (
         <div className="wrapper">
-            <h1>React and TypeScript App!🚀</h1>
+            <h1>React 18 and TypeScript 5 App!🚀</h1>
         </div>
     );
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,41 +1,66 @@
-import path from "path";
-import {Configuration as WebpackConfiguration, DefinePlugin} from 'webpack';
+import Dotenv from 'dotenv-webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
+import {Configuration as WebpackConfiguration} from 'webpack';
 import {Configuration as WebpackDevServerConfiguration} from 'webpack-dev-server';
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
 
 interface Configuration extends WebpackConfiguration {
     devServer?: WebpackDevServerConfiguration;
 }
 
-const webpackConfig = (): Configuration => {
-    return ({
-        entry: "./src/index.tsx",
-        ...(process.env.production || !process.env.development
-            ? {}
-            : {devtool: "eval-source-map"}),
+const devPlugins: Configuration['plugins'] = [
+    new Dotenv(),
+    new HtmlWebpackPlugin({
+        template: './public/index.html',
+    }),
+];
 
+const webpackConfig = (env: any, argv: any): Configuration => {
+    const isDevMode = argv?.mode !== 'production';
+
+    console.log('isDevMode?', isDevMode);
+
+    return {
+        entry: './src/index.tsx',
+        mode: (argv.mode as 'production' | 'development' | undefined) ?? 'development',
+        ...(isDevMode
+            ? {devtool: 'eval-source-map'}
+            : {
+                  performance: {
+                      hints: false,
+                      maxEntrypointSize: 500 * 1024,
+                      maxAssetSize: 500 * 1024,
+                  },
+              }),
         resolve: {
-            extensions: [".ts", ".tsx", ".js"],
-            plugins: [new TsconfigPathsPlugin({configFile: "./tsconfig.json"})],
+            extensions: ['.ts', '.tsx', '.js'],
+            plugins: [new TsconfigPathsPlugin({configFile: './tsconfig.json'})],
         },
         output: {
-            path: path.join(__dirname, "/build"),
-            filename: "build.js",
+            path: path.join(__dirname, '/build'),
+            filename: 'build.[hash:8].js',
         },
         module: {
             rules: [
                 {
-                    test: /\.tsx?$/,
-                    loader: "ts-loader",
+                    test: /\.(ts|tsx)$/,
+                    loader: 'ts-loader',
                     options: {
                         transpileOnly: true,
+                        configFile: path.resolve(__dirname, './tsconfig.json'),
                     },
                     exclude: /build/,
                 },
                 {
-                    test: /\.s?css$/,
-                    use: ["style-loader", "css-loader"],
+                    test: /\.(sa|sc|c)ss$/i,
+                    use: [
+                        isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'postcss-loader',
+                        'sass-loader',
+                    ],
                 },
             ],
         },
@@ -44,15 +69,10 @@ const webpackConfig = (): Configuration => {
             open: true,
             historyApiFallback: true,
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: "./public/index.html",
-            }),
-            new DefinePlugin({
-                "process.env": process.env.production || !process.env.development,
-            }),
-        ],
-    });
+        plugins: isDevMode
+            ? devPlugins
+            : [...devPlugins, new MiniCssExtractPlugin({filename: 'styles.[hash:8].css'})],
+    };
 };
 
 export default webpackConfig;
